ba0a2d8aea33e1d122fb471312812e3211dc54dc
Evan Timmons <evangeorgetimmons@gmail.com>
2020-09-23 11:11:52 -0500

    Programmed to size and resolution of prototype display. Will also open fullsize and automatically

--- MainGUI.py
+++ MainGUI.py
@@ -13,9 +13,9 @@ menu_def = [['File', ['Open', 'Save', 'Exit', 'Properties']],
 
 # ------ Column Definition ------ #
 column1 = [[sg.Text('Column 1', background_color='#F7F3EC', justification='center', size=(10, 1))],
-            [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 1')],
-            [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 2')],
-            [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 1')],
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 2')],
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
 
 layout = [
     [sg.Menu(menu_def, tearoff=True)],
@@ -23,34 +23,41 @@ layout = [
     [sg.Text('Here is some text.... and a place to enter text')],
     [sg.InputText('This is my text')],
     [sg.Frame(layout=[
-    [sg.Checkbox('Checkbox', size=(10,1)),  sg.Checkbox('My second checkbox!', default=True)],
-    [sg.Radio('My first Radio!     ', "RADIO1", default=True, size=(10,1)), sg.Radio('My second Radio!', "RADIO1")]], title='Options',title_color='red', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],
+        [sg.Checkbox('Checkbox', size=(10, 1)), sg.Checkbox('My second checkbox!', default=True)],
+        [sg.Radio('My first Radio!     ', "RADIO1", default=True, size=(10, 1)),
+         sg.Radio('My second Radio!', "RADIO1")]], title='Options', title_color='red', relief=sg.RELIEF_SUNKEN,
+        tooltip='Use these to set flags')],
     [sg.Multiline(default_text='This is the default Text should you decide not to type anything', size=(35, 3)),
-        sg.Multiline(default_text='A second multi-line', size=(35, 3))],
+     sg.Multiline(default_text='A second multi-line', size=(35, 3))],
     [sg.InputCombo(('Combobox 1', 'Combobox 2'), size=(20, 1)),
-        sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],
+     sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],
     [sg.InputOptionMenu(('Menu Option 1', 'Menu Option 2', 'Menu Option 3'))],
     [sg.Listbox(values=('Listbox 1', 'Listbox 2', 'Listbox 3'), size=(30, 3)),
-        sg.Frame('Labelled Group',[[
-        sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=25),
-        sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=75),
-        sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),
-        sg.Column(column1, background_color='#F7F3EC')]])],
-    [sg.Text('_'  * 80)],
+     sg.Frame('Labelled Group', [[
+         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=25),
+         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=75),
+         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),
+         sg.Column(column1, background_color='#F7F3EC')]])],
+    [sg.Text('_' * 80)],
     [sg.Text('Choose A Folder', size=(35, 1))],
     [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
-        sg.InputText('Default Folder'), sg.FolderBrowse()],
+     sg.InputText('Default Folder'), sg.FolderBrowse()],
     [sg.Submit(tooltip='Click to submit this window'), sg.Cancel()]
 ]
 
+# Keep no_titlebar == True to disable title bar
+# Dimensions hardcoded to screen values
+window = sg.Window('Sparrow v1', layout, no_titlebar=True, default_element_size=(40, 1), grab_anywhere=False,
+                   location=(0, 0), size=(1024, 600), keep_on_top=True).Finalize()
 
-window = sg.Window('Sparrow v1', layout, default_element_size=(40, 1), grab_anywhere=False)
+# Maximize the window automatically
+# window.Maximize()
 
 event, values = window.read()
 
 window.close()
 
 sg.popup('Title',
-            'The results of the window.',
-            'The button clicked was "{}"'.format(event),
-            'The values are', values)
+         'The results of the window.',
+         'The button clicked was "{}"'.format(event),
+         'The values are', values)
