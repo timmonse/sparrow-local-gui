83bd1510dfe18b3cc0eda54d71325f1858e2f9ab
Evan G Timmons <evangeorgetimmons@gmail.com>
2020-10-22 09:37:12 -0500

    Add files via upload

--- MainGUI.py
+++ MainGUI.py
@@ -37,18 +37,18 @@ layout = [
 # Keep no_titlebar == True to disable title bar
 # Dimensions hardcoded to screen values
 # Add element_justification='c' to center the elements
-window = sg.Window('Sparrow v1', layout, no_titlebar=False, default_element_size=(40, 1), grab_anywhere=False,
-                   location=(0, 0), size=(1024, 600), keep_on_top=True)
+window = sg.Window('Sparrow v1', layout, no_titlebar=True, default_element_size=(40, 1), grab_anywhere=False,
+                   location=(0, 0), size=(1024, 600), keep_on_top=False).Finalize()
 
 # Maximize the window automatically
-# window.Maximize()
+window.Maximize()
 
 # Terminal command tests
 # list_files = subprocess.run(["ls", "-l"])
 # print("The exit code was: %d" % list_files.returncode)
 
 # event, values = window.read()
-cap = cv2.VideoCapture(0)  # Setup the OpenCV capture device (webcam)
+cap = cv2.VideoCapture(2)  # Setup the OpenCV capture device (webcam)
 timeout = 20
 isVisible = False
 output = "none"
