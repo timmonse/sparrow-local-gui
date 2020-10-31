1fe9f0a3be7e5594fc872735d01ea17b8d3ca469
Evan Timmons <evangeorgetimmons@gmail.com>
2020-10-31 13:53:06 -0500

    Removed dummy buttons, added upload button and centered elements

--- SparrowV2.py
+++ SparrowV2.py
@@ -18,16 +18,14 @@ menu_def = [['File', ['Open', 'Save', 'Exit', 'Properties']],
 layout = [
     [sg.Menu(menu_def, tearoff=True)],
     [sg.Text('Sparrow', size=(55, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
-    [sg.Text('Video Storage Location', size=(17, 1), auto_size_text=False, justification='left'),
-     sg.InputText('Default Folder'), sg.FolderBrowse()],
     [sg.Image(filename='', key='_IMAGE_', size=(10, 10)),
      sg.Frame('Display Video Feed', [[
          sg.Button('Hide/Show', button_color=('white', 'Black'))
      ]]),
-     sg.Frame('Camera Controls', [[
-         sg.Button('Record', button_color=('white', 'red')), sg.Button('Pause', button_color=('black', 'yellow')),
-         sg.Button('Resume', button_color=('white', 'green')), sg.Button('Stop', button_color=('white', 'black'))
-     ], [sg.Text('Put the next row here')]])
+     sg.Frame('Control Panel', [[
+         sg.Button('Record', button_color=('white', 'red')),
+         sg.Button('Stop', button_color=('white', 'black'))
+     ], [sg.Button('Upload', button_color=('white', 'green'))]])
      ],
     [sg.Submit(tooltip='Click to submit this window'), sg.Cancel()]
 ]
@@ -36,13 +34,13 @@ layout = [
 # Dimensions hardcoded to screen values
 # Add element_justification='c' to center the elements
 window = sg.Window('Sparrow v2', layout, no_titlebar=False, default_element_size=(40, 1), grab_anywhere=False,
-                   location=(0, 0), size=(1024, 600), keep_on_top=False).Finalize()
+                   location=(0, 0), size=(1024, 600), keep_on_top=False, element_justification='c').Finalize()
 
 # Maximize the window automatically
 window.Maximize()
 
 # event, values = window.read()
-cap = cv2.VideoCapture(2)  # Setup the OpenCV capture device (webcam)
+cap = cv2.VideoCapture(0)  # Setup the OpenCV capture device (webcam)
 
 timeout = 20
 isVisible = False
@@ -58,14 +56,12 @@ while True:
     frame = imutils.resize(frame, width=520)
     imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # Convert the image to PNG Bytes
     window.FindElement('_IMAGE_').Update(data=imgbytes)  # Change the Image Element to show the new image
-    if event == "Pause" or event == "Stop":
+    if event == "Stop":
         # window.FindElement('_IMAGE_').Update(visible=False)
         # Pause and stop logic to go here
         for child in children:
             os.killpg(os.getpgid(child.pid), signal.SIGTERM)
         timeout = 10000000
-    if event == "Resume":
-        timeout = 20
     if event == "Hide/Show":
         window.FindElement('_IMAGE_').Update(visible=isVisible)
         isVisible = not isVisible
